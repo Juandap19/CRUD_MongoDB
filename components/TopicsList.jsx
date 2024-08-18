fd2a31f71ee44b05ad03dd6afa1cb105ffc34d18
Juandap19 <juanda232405@hotmail.com>
2024-08-18 14:35:18 -0500

    adding some configurations to deploy 🤓

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -4,9 +4,12 @@ import { HiPencilAlt } from "react-icons/hi";
 
 const getTopics = async () => {
   try {
-    const res = await fetch("http://localhost:3000/api/topics", {
-      cache: "no-store",
-    });
+    const res = await fetch(
+      "https://crud-mongo-db-alpha.vercel.app/api/topics",
+      {
+        cache: "no-store",
+      }
+    );
 
     if (!res.ok) {
       throw new Error("Failed to fetch topics");
@@ -19,30 +22,29 @@ const getTopics = async () => {
 };
 
 export default async function TopicsList() {
-  let topics;
-  topics = await getTopics();
+  const {topics} = await getTopics();
 
   return (
     <>
       {topics &&
-      topics.map((t) => (
-        <div
-          key={t._id}
-          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
-        >
-          <div>
-            <h2 className="font-bold text-2xl">{t.title}</h2>
-            <div>{t.description}</div>
-          </div>
+        topics.map((t) => (
+          <div
+            key={t._id}
+            className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
+          >
+            <div>
+              <h2 className="font-bold text-2xl">{t.title}</h2>
+              <div>{t.description}</div>
+            </div>
 
-          <div className="flex gap-2">
-            <RemoveBtn id={t._id} />
-            <Link href={`/editTopic/${t._id}`}>
-              <HiPencilAlt size={24} />
-            </Link>
+            <div className="flex gap-2">
+              <RemoveBtn id={t._id} />
+              <Link href={`/editTopic/${t._id}`}>
+                <HiPencilAlt size={24} />
+              </Link>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </>
   );
 }
